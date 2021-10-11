71f69738995b6dc883d5fa5e2c0e853a0360e69f
Adi-mor2009 <adijmy@gmail.com>
2021-10-11 14:49:42 +0300

    routes basic pages + cleanups

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage(props) {
+    return (
+        <div className="p-home">
+            HomePage
+        </div>
+    );
+}
+
+export default HomePage;
